2fa4890471cdffbe3fdc11ba297bc80081066e1d
nonoshita4 <nonoshita4@gmail.com>
2024-06-18 23:33:58 +0900

    Create statement.js

---
+++ statement.js
@@ -0,0 +1,31 @@
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      default:
+        throw new Error("unknown type: ${play.type}");
+    }
+  }
+}
