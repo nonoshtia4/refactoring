66db9655f2952eb7181602edfd445b3ed662ccfe
nonoshita4 <nonoshita4@gmail.com>
2024-06-25 07:03:41 +0900

    sepalate statement function

--- statement.js
+++ statement.js
@@ -10,29 +10,34 @@ function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      default:
-        throw new Error("unknown type: ${play.type}");
-    }
     volumeCredits += Math.max(perf.audience - 30, 0);
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
     result += `${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats) \n`;
+    thisAmount = amountFor(perf, play);
     totalAmount += thisAmount;
   }
   result += `Amount owed is ${format(totalAmount / 100)}`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+}
+
+function amountFor(perf, play) {
+  let thisAmount = 0;
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    default:
+      throw new Error("unknown type: ${play.type}");
+  }
+  return thisAmount;
 }
